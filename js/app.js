11739117a4b0781c44ebb69fe8d55caf64600ebd
Jay Schwane <jrschwane@uwalumni.com>
2017-03-06 18:48:46 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+/* jshint esversion:6 */
+/* globals jQuery, console, RPN */
+
+/*
+   Javascrit Pomodoro-style timer
+
+*/
+
+(function ($) {
+    
+    'use strict';
+    
+    var DOM       = {};   // cached DOM elements
+        
+
+    // cache DOM elements
+    function cacheDom() {
+        
+    }
+
+    
+    // bind events
+    function bindEvents() {
+        
+    }
+    
+    
+    // render DOM
+    function render() {
+        
+    }
+    
+
+    // auto-init on page load
+    (function init() {
+        cacheDom();
+        bindEvents();
+    }());
+    
+}(jQuery));
